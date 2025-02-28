4f3a4009c870f4378fbf39d7c786e3761b7eb046
shisui06 <127260275+shisui06@users.noreply.github.com>
2025-02-27 19:53:48 -0500

    Aboutpage syntax error

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "acfc253cba67b01d"; }
+/******/ 		__webpack_require__.h = function() { return "c76b8833389fcc7e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
-import AboutSection from "./components/aboutSection";
+import AboutSection from "./components/AboutSection";
 import ProjectsSection from "./components/ProjectsSection";
 import EmailSection from "./components/EmailSection";
 import Footer from "./components/Footer";
